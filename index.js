a424678e909e4dc8952af4dc9de45c647816b1ad
Ruban <80041905+RubanN@users.noreply.github.com>
2021-06-05 21:53:45 +0530

    javascript learning

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const myFunction =(n)=>{
-   n=n+"";
-   return n.split('').reverse().join('');
-   
+// Head First javasript 
+var age =14;
+var name = 'Owen';
+if(age>14){
+   // console.log('you are not fit ');
+}else{
+   // console.log('sorry'+name+"for anything");
 }
-console.log(myFunction(1234));
+var price=20;
+var discount=6;
+var total = price- (price* (discount/100));
+var dogName = "rover";
+var dogWeight = 23;
+if(dogWeight>20){
+   // console.log(dogName+'says wOOF wOOF');
+}else{
+   // console.log(dogName+"says woof woof");
+}
+dogName='spot';
+dogWeight=13;
+
+if(dogWeight>20){
+   // console.log(dogName+"says WOOF");
+}
